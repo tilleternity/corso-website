22a331c8652e04b879b877c39a627f95afaac1a3
MacBook <macbook@MacBooks-MacBook-Pro.local>
2024-04-24 13:48:27 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function userscroll() {
+    const navbar = document.querySelector('.navbar');
+
+    window.addEventListener('scroll', ()=> {
+        if (window.scrollY > 50){
+            navbar.classList.add('bg-dark');
+        }else {
+            navbar.classList.remove('bg-dark');
+        }
+    } );
+
+}
+document.addEventListener('DOMContentLoaded',
+userscroll);
